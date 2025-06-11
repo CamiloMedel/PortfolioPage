0f531b239322b20c618d907807ad91ee472d8969
CamiloMedel <129447046+CamiloMedel@users.noreply.github.com>
2025-06-11 17:42:24 -0500

    animated contact > confirmation redirection

--- src/components/Contact/ContactConfirmation.jsx
+++ src/components/Contact/ContactConfirmation.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router';
 import './ContactConfirmation.css';
+import 'animate.css';
 
 function ContactConfirmation() {
     return (
         <main className='contact-confirmation-container'>
-            <div className='text-container'>
+            <div className='text-container animate__animated animate__fadeIn'>
                 <h1 className='title'>Contact Message Sent!</h1>
                 <Link to='/' className='home-button'>Home</Link>
             </div>
