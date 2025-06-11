95c07b2da9e3c5516641fc1fe28fae5c534be320
CamiloMedel <129447046+CamiloMedel@users.noreply.github.com>
2025-06-11 17:16:51 -0500

    contact page redirect update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,8 @@ import './Contact.css'
 function Contact(){
     return (
         <main className="contact-container">
-            <form action="https://formsubmit.co/1037577e1ba38bca3872f4dcda3a16ab" method="POST" data-redirect="https://camilomedel.github.io/PortfolioPage/" className='animate__animated animate__fadeIn'>
+            <form action="https://formsubmit.co/1037577e1ba38bca3872f4dcda3a16ab" method="POST" className='animate__animated animate__fadeIn'>
+                <input type="hidden" name="_next" value="https://camilomedel.github.io/PortfolioPage/"/>
                 <input type='text' name='_honey' style={{display: 'none'}}/>
                 <h1 className="title">Contact</h1>
                 <label for='name'>Name:</label>
