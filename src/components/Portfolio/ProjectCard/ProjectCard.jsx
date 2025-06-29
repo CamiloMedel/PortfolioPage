74c0403f90a64a5593ac8c907247d5be5b330462
CamiloMedel <129447046+CamiloMedel@users.noreply.github.com>
2025-06-28 21:13:14 -0500

    project card link UX update

--- src/components/Portfolio/ProjectCard/ProjectCard.jsx
+++ src/components/Portfolio/ProjectCard/ProjectCard.jsx
@@ -9,7 +9,9 @@ function ProjectCard({projectInfo}) {
                 <img src={projectInfo.coverPhoto} alt={`Photo showcasing ${projectInfo.title}`}/>
             </Link>
             <div className='project-card-text'>
-                <h2>{projectInfo.title}</h2>
+                <Link className='project-text-link' to={projectInfo.link}>
+                    <h2>{projectInfo.title}</h2>
+                </Link>
                 <p>{projectInfo.description}</p>
                 <div className='tag-container'>
                     {projectInfo.tags.map((label) => {
